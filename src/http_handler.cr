28822a31d7fc7cf2484e997c4811fcf63b5c77da
Jamie Gaskins <jgaskins@hey.com>
2025-01-07 14:31:28 -0600

    http.response.status is an integer
    I intended to provide both the numeric code and the HTTP status message
    but accidentally encoded both of them the same. Oops!

--- src/http_handler.cr
+++ src/http_handler.cr
@@ -22,7 +22,7 @@ class Honeybadger::Integrations::HTTPHandler
         trace_id: Fiber.current.honeybadger_trace_id,
         "http.request.method": context.request.method,
         "http.request.resource": context.request.resource,
-        "http.response.status": context.response.status,
+        "http.response.status": context.response.status_code,
         "http.response.status_message": context.response.status.to_s,
         duration_ms: (Time.monotonic - start).total_milliseconds,
         error: error,
